891e5e6d877c09ffb69c09d016d01e467b25691c
Vladimir <grivle@mail.ru>
2024-06-11 16:01:09 +0700

    add a proxy for receiving API to vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,22 @@ import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      "/ipify": {
+        target: 'https://api.ipify.org?format=json',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/ipify/, ""),
+      }
+      // "/dadata": {
+      //   target: 'http://suggestions.dadata.ru/suggestions/api/4_1/rs/iplocate/address',
+      //   changeOrigin: true,
+      //   secure: false,
+      //   rewrite: (path) => path.replace(/^\/dadata/, ""),
+      // }
+    }
+  },
   plugins: [react(), svgr()],
   resolve: {
     alias: {
